5c271c2861f6f6d10fae53574f34bf7abde400e5
StephaneLay <stephane.lay@hotmail.fr>
2025-06-27 14:03:05 +0200

    new fixtures functional

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -14,37 +14,49 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        $albums = json_decode(file_get_contents(__DIR__ . "/data/albums.json"), true);
-        $artists = json_decode(file_get_contents(__DIR__ . "/data/artists.json"), true);
-        $genres = json_decode(file_get_contents(__DIR__ . "/data/genres.json"), true);
-        $songs = json_decode(file_get_contents(__DIR__ . "/data/songs.json"), true);
+        $dataAlbums = json_decode(file_get_contents(__DIR__ . "/data/albums.json"), true);
+        $dataArtists = json_decode(file_get_contents(__DIR__ . "/data/artists.json"), true);
+        $dataGenres = json_decode(file_get_contents(__DIR__ . "/data/genres.json"), true);
+        $dataSongs = json_decode(file_get_contents(__DIR__ . "/data/songs.json"), true);
 
-        for ($i = 0; $i < count($albums); $i++) {
+        $albums = [];
+        $artists = [];
+        $genres = [];
+
+        foreach ($dataGenres as $genreline) {
             $genre = new Genre();
-            $artist = new Artist();
-            $album = new Album();
-
-            $genre->setName($genres[$i]["name"]);
+            $genre->setName($genreline["name"]);
             $manager->persist($genre);
-
-            $artist->setName($artists[$i]["name"])
-                ->setDescription($artists[$i]["description"])
-                ->setMemberNumber($artists[$i]["member_number"])
-                ->setGenre($genre);
-            $manager->persist($artist);
-
-            $album->setName($albums[$i]["name"])
-                ->setCreatedAt(new DateTimeImmutable($albums[$i]["created_at"]))
-                ->setArtist($artist);
-            $manager->persist($album);
-
-            $song = new Song();
-            $song->setName($songs[$i]["name"])
-                ->setLengthSecond($songs[$i]["length_second"])
-                ->setAlbum($album);
-            $manager->persist($song);
+            $genres[] = $genre;
         }
 
+        foreach ($dataArtists as $artistLine) {
+            $artist = new Artist();
+            $artist->setName($artistLine["name"])
+                ->setDescription($artistLine["description"])
+                ->setMemberNumber($artistLine["member_number"])
+                ->setGenre($genres[$artistLine["genre_id"]-1]);
+            $manager->persist($artist);
+            $artists[] = $artist;
+        }
+
+        foreach ($dataAlbums as $albumLine) {
+            $album = new Album();
+            $album->setName($albumLine["name"])
+            ->setCreatedAt(new DateTimeImmutable($albumLine["created_at"]))
+            ->setArtist($artists[$albumLine["artist_id"]-1]);
+            $manager->persist($album);
+            $albums[] = $album;
+        }
+
+        foreach ($dataSongs as $songLine) {
+            $song = new Song();
+            $song->setName($songLine["name"])
+            ->setLengthSecond( $songLine["duration"])
+            ->setAlbum($albums[$songLine["album_id"]-1]);
+            $manager->persist($song);
+            
+        }
 
         $manager->flush();
     }
